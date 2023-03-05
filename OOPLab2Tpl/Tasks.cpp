7ef0f4ecb4a83872700393530c5ace0dcae54b6f
ChervinskaAnna <chervinska.anna@chnu.edu.ua>
2023-03-05 13:48:26 +0200

    task 1

--- OOPLab2Tpl/Tasks.cpp
+++ OOPLab2Tpl/Tasks.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <stdio.h>
+#include <conio.h>
 using namespace std;
 #include "Tasks.h"
 #include "Examples.h"
@@ -16,6 +18,28 @@ void task1() {
     // Обчислення виразів з використанням побітових операцій
     // Calculation of expressions using bitwise operations 
     cout << " Calculation of expressions using bitwise operations  \n";
+        int a, b, c, d;
+        int ABCD, ACDB;
+        cout << "ACDB = (127 * a + 32 * c) / 4096 - d * 1200 + b * 131" << endl;
+        cout << "Enter a" << endl;
+        cin >> a;
+
+        cout << "Enter b" << endl;
+        cin >> b;
+
+        cout << "Enter c" << endl;
+        cin >> c;
+
+        cout << "Enter d" << endl;
+        cin >> d;
+
+        ACDB = (127 * a + 32 * c) / 4096 - d * 1200 + b * 131;
+        ABCD = (((a << 7) - a + (c << 5)) >> 12) - (d << 10) - (d << 7) - (d << 5) - (d << 4) + (b << 7) + (b << 1) + b;
+
+        cout << "RES" << endl;   
+        cout << ACDB << endl;
+        cout << ABCD << endl;
+        _getch();
 }
 
 void task2()
